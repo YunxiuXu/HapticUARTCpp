3320174c67aec990d8e20d54d69ef9847e37fa77
YunxiuXu <60459891+YunxiuXu@users.noreply.github.com>
2023-07-04 17:11:58 +0900

    拆分版uart

--- HapticUART/HapticUART.cpp
+++
@@ -1,7 +0,0 @@
-#include "GetSocket.h"
-
-int main()
-{
-    runServer();
-    return 0;
-}
---
+++ HapticUART/Main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+#include "serial.h"
+
+#define M_PI 3.14159265
+
+int main()
+{
+    double frequency = 3; // 调整这个值以改变频率
+
+    SerialPort serial("COM3", 2000000); // 创建串口对象
+
+    if (!serial.isOpen()) {
+        std::cerr << "ERROR: Unable to open serial port.\n";
+        return 1;
+    }
+
+    auto start_time = std::chrono::steady_clock::now();
+    auto next_time = start_time;
+
+    while (true) {
+        /////////////////////////Code for socket
+        
+        /////////////////////////Code for serial
+        next_time += std::chrono::milliseconds(1);
+        double elapsed_time = std::chrono::duration<double>(next_time - start_time).count();
+
+        int data_to_send = ((127 * (std::sin(2 * M_PI * frequency * elapsed_time) + 1.0)));
+        data_to_send = static_cast<float>(data_to_send) / 255 * 300;
+
+        auto result = intToHexProtocol(data_to_send);
+
+        //unsigned char data_to_sends[] = { 0x31, result[0], result[1] };
+        unsigned char data_to_sends[] = { 0x31, result[0], result[1], 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        DWORD bytes_to_send = sizeof(data_to_sends);
+        DWORD bytes_written = 0;
+
+        if (!serial.writeData(data_to_sends, bytes_to_send)) {
+            std::cerr << "ERROR: Unable to write to serial port.\n";
+            return 1;
+        }
+
+        if (bytes_written != bytes_to_send) {
+            std::cerr << "WARNING: Not all bytes were written to serial port.\n";
+        }
+
+        std::this_thread::sleep_until(next_time);
+
+    }
+
+    return 0;
+}
